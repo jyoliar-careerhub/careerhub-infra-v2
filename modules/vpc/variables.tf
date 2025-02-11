c6180dcc6c4a71e17e2a22b7d20466f97060dc24
jyo liar <jyo-liar@gmail.com>
2025-02-11 16:25:55 +0900

    vpc 모듈 코드 추가

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,55 @@
+variable "name" {
+  description = "The name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+
+
+variable "instance_tenancy" {
+  description = "A tenancy option for instances launched into the VPC"
+  type        = string
+  default     = "default"
+}
+
+variable "enable_dns_hostnames" {
+  description = "Should be true to enable DNS hostnames in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "map_public_ip_on_launch" {
+  description = "Specify true to indicate that instances launched into the subnet should be assigned a public IP address. Default is `true`"
+  type        = bool
+  default     = true
+}
+
+variable "public_subnets" {
+  type = list(object({
+    cidr_block        = string
+    availability_zone = optional(string)
+  }))
+}
+
+variable "public_subnet_suffix" {
+  description = "Suffix to append to public subnets name"
+  type        = string
+  default     = "public"
+}
+
+variable "private_subnets" {
+  type = list(object({
+    cidr_block        = string
+    availability_zone = optional(string)
+  }))
+}
+
+variable "private_subnet_suffix" {
+  description = "Suffix to append to private subnets name"
+  type        = string
+  default     = "private"
+}
