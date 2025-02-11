46ab21a6203e5e5244290c35925025fca1e54d3a
jyo liar <jyo-liar@gmail.com>
2025-02-11 18:22:19 +0900

    vpc 관련 정보 output 처리

---
+++ prod/vpc/outputs.tf
@@ -0,0 +1,29 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
+}
+
+output "public_subnet_objects" {
+  description = "A list of all public subnets, containing the full objects."
+  value       = module.vpc.public_subnet_objects
+}
+
+output "private_subnet_objects" {
+  description = "A list of all private subnets, containing the full objects."
+  value       = module.vpc.private_subnet_objects
+}
+
+output "public_route_table_id" {
+  description = "The ID of the public route table"
+  value       = module.vpc.public_route_table_id
+}
+
+output "private_route_table_id" {
+  description = "The ID of the private route table"
+  value       = module.vpc.private_route_table_id
+}
