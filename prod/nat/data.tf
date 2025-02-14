d13a243895c80e9f1a75ce5d1a4328b54617ed72
jyo liar <jyo-liar@gmail.com>
2025-02-14 18:03:13 +0900

    잘못된 변수 참조 수정

--- prod/nat/data.tf
+++ prod/nat/data.tf
@@ -5,9 +5,9 @@ locals {
 module "remote_state" {
   source = "../../_modules/tfc_remote_state"
 
-  workspaces = [vpc_ws]
+  workspaces = [local.vpc_ws]
 }
 
 locals {
-  vpc_outputs = module.remote_state.outputs[vpc_ws]
+  vpc_outputs = module.remote_state.outputs[local.vpc_ws]
 }
