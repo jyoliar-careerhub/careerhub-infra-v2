31924ded970e51244d08e990f5200ef62e8352f8
jyo liar <jyo-liar@gmail.com>
2025-02-14 18:15:17 +0900

    output 내용 체크

--- prod/nat/data.tf
+++ prod/nat/data.tf
@@ -8,6 +8,10 @@ module "remote_state" {
   workspaces = [local.vpc_ws]
 }
 
+output "vpc_outputs" {
+  value = module.remote_state.outputs
+}
+
 locals {
   vpc_outputs = module.remote_state.outputs[local.vpc_ws]
 }
