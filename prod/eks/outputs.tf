48c4147fe8af653c6883fcf1d18e5babade85162
jyo liar <jyo-liar@gmail.com>
2025-02-15 17:25:24 +0900

    eks output 수정

--- prod/eks/outputs.tf
+++ prod/eks/outputs.tf
@@ -1,3 +1,11 @@
-output "eks" {
-  value = module.eks
+output "eks_cluster_name" {
+  value = module.eks.eks_cluster_name
+}
+
+output "vpc_id" {
+  value = local.vpc_outputs.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = local.vpc_outputs.public_subnet_ids
 }
