790a629bf36c9bd619638f93f16d81c150d82362
jyo liar <jyo-liar@gmail.com>
2025-02-17 16:25:06 +0900

    외부 워크스페이스에서 테라폼 자격증명이 eks cluster를 제어하기 위한 권한 추가

--- prod/eks/main.tf
+++ prod/eks/main.tf
@@ -5,3 +5,17 @@ module "eks" {
   subnet_ids = local.vpc_outputs.public_subnet_ids
   vpc_id     = local.vpc_outputs.vpc_id
 }
+
+data "aws_caller_identity" "terraform" {}
+
+data "aws_iam_role" "terraform" {
+  name = element(split("/", data.aws_caller_identity.terraform.arn), 1)
+}
+
+module "eks_access" {
+  source = "../../_modules/eks_access"
+
+  cluster_name = module.eks.eks_cluster_name
+
+  cluster_admin_arns = [data.aws_iam_role.terraform.arn]
+}
