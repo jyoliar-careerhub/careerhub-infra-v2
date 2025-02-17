57a914dd4446c6a1ae3734fd582d86d533d29a96
jyo liar <jyo-liar@gmail.com>
2025-02-17 19:21:22 +0900

    irsa 관련 모듈 작성 진행중

---
+++ _modules/role_for_sa/main.tf
@@ -0,0 +1,27 @@
+locals {
+  temps             = split("/", var.eks_oidc_provider_arn)
+  oidc_provider_url = join("/", slice(local.temps, 1, length(local.temps)))
+}
+resource "aws_iam_role" "this" {
+  name = "${var.namespace}-${var.service_account_name}-eks-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRoleWithWebIdentity",
+        ]
+        Effect = "Allow"
+        Principal = {
+          Federated = var.eks_oidc_provider_arn
+        }
+        Condition = {
+          StringEquals = {
+            "${local.oidc_provider_url}:sub" = "system:serviceaccount:${var.namespace}:${var.service_account_name}"
+            "${local.oidc_provider_url}:aud" = "sts.amazonaws.com"
+          }
+        }
+      },
+    ]
+  })
+}
---
+++ _modules/role_for_sa/variables.tf
@@ -0,0 +1,11 @@
+variable "eks_oidc_provider_arn" {
+  type = string
+}
+
+variable "namespace" {
+  type = string
+}
+
+variable "service_account_name" {
+  type = string
+}
