079f395f61472ff017140e18942a8f4d74705681
jyo liar <jyo-liar@gmail.com>
2025-02-17 16:28:35 +0900

    eks 워크스페이스에서 테라폼 자격증명에 admin권한을 default로 부여함에 따라 불필요한 권한부여 코드 제거

--- prod/eks_access/main.tf
+++ prod/eks_access/main.tf
@@ -1,14 +1,8 @@
-data "aws_caller_identity" "terraform" {}
-
-data "aws_iam_role" "terraform" {
-  name = element(split("/", data.aws_caller_identity.terraform.arn), 1)
-}
-
 module "eks_access" {
   source = "../../_modules/eks_access"
 
   cluster_name = local.eks_outputs.eks_cluster_name
 
-  cluster_admin_arns = concat(var.cluster_admin_arns, [data.aws_iam_role.terraform.arn])
+  cluster_admin_arns = var.cluster_admin_arns
   access_entries     = var.access_entries
 }
