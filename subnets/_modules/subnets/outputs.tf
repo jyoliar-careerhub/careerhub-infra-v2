0c797f58c20ae9cef5f40d62e885c42e93efd60c
jyo liar <jyo-liar@gmail.com>
2025-04-08 14:59:03 +0900

    subnets 모듈 추가

---
+++ subnets/_modules/subnets/outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.this.id
+}
+
+output "public_subnet_objects" {
+  description = "A list of all public subnets, containing the full objects."
+  value       = aws_subnet.public
+}
+
+output "public_route_table_id" {
+  description = "The ID of the public route table"
+  value       = var.public_route_table_id
+}
+
+output "private_subnet_objects" {
+  description = "A list of all private subnets, containing the full objects."
+  value       = aws_subnet.private
+}
+
+output "private_route_table_id" {
+  description = "The ID of the private route table"
+  value       = var.private_route_table_id
+}
